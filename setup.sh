c329ae191169f967bf7424800f39f4d55316a47b
Takuya Ookubo <sfio@sakura.ai.to>
2011-08-21 08:35:21 +0900

    v0.21のfluxflex対応 http://rm-ameria-jp.fluxflex.com/issues/2

--- setup.sh
+++ setup.sh
@@ -1,10 +1,2 @@
-#!/bin/sh
+#!/bin/sh -
 
-#public_html/install/go-pear.phar list-packages
-#pear channel-discover pear.ethna.jp
-#pear update-channels
-#pear install -a ethna/ethna
-
-mysql -h FLX_DB_HOST -u FLX_DB_USER FLX_DB_NAME < schema/example_bbs_mysql.sql
-mysql -h FLX_DB_HOST -u FLX_DB_USER FLX_DB_NAME < schema/example_mysql.sql
-mysql -h FLX_DB_HOST -u FLX_DB_USER FLX_DB_NAME < schema/session_mysql.sql
